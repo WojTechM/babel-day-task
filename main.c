84778f90b46d69ad8196d7b1248bd6d53cd28db9
ZupaRomana <adam.romanowski93@gmail.com>
2019-02-22 10:50:22 +0100

    No idea what im doing

--- main.c
+++ main.c
@@ -2,6 +2,33 @@
 #include <stdlib.h>
 
 int main() {
-   printf("Hello world");
-   return 0;
+
+    char ch;
+    FILE *fp;   // some pointers happens there
+    char *yourFullPath = "/home/roman/babel/babel-day-task/en_full.txt"; // your full path to file
+    char fileContent[100000] = "";  // set to 100000 just in care
+
+    fp = fopen(yourFullPath, "r"); // read mode
+
+    if (fp == NULL)
+    {
+        perror("Error while opening the file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int i = 0;
+    while((ch = (char) fgetc(fp)) != EOF) {
+        fileContent[i] = ch;
+        i++;
+    }
+
+    fclose(fp);
+    printf("%s", fileContent);
+
+    printf("The longest word: \n");
+    printf("The shortest word: \n");
+    printf("Average word length: \n");
+    printf("Number of words: \n");
+
+    return 0;
 }
